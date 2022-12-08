0420078738e27b142080c0f13426c71ac94a0dce
ndandax <sebandandala99@gmail.com>
2022-12-08 12:27:44 -0500

    c1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: 'description'
+*
+Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n";
-        return (0);
- }
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
