b9b9d4013e15d238168ab834d2bcaa01a643847b
ndandax <sebandandala99@gmail.com>
2022-12-13 11:28:25 -0500

    c3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Entry point
+ *Description: program that prints lower case letters
+ *
+ *Return: 0
+ */
+void print_albhabet(void)
+{
+	int a;
+
+	for (a = "a"; a <= "z"; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,12 @@
 #define MAIN_H
 
 int _putchar(char c);
+void print_alphabet(void);
+void print_albhabet_x10(void);
+int  _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
 
 #endif
