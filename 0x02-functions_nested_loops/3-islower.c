3b7d1d17a904a17bda719af86f109bc2678c1a4c
ndandax <sebandandala99@gmail.com>
2022-12-13 12:45:20 -0500

    c8

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_islower - Entry point
+ *Description: checks lowercase
+ *
+ * @c character to check
+ *Return: 1 if lowercase , 0 if uppercase
+ */
+int _islower(int 0)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isalpha - Entry point
+ *
+ *Return: 1 if lower or uppercase, 0 if not alphabet
+ */
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	putchar('\n);
+}
