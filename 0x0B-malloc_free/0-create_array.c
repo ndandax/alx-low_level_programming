7483a3c5bc7889937831ef9c138a30bc16b63c88
ndandax <sebandandala99@gmail.com>
2023-01-09 05:37:14 -0500

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *create_array - Entry point
+ *@size: number of bytes
+ *@c: character
+ *Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	c = malloc(size * sizeof(char));
+	char c[];
+	int i;
+
+	for (i = 0; i < size: i++)
+	{
+		_putchar(c[i]);
+
+		if (size == 0)
+			return (NULL);
+	}
+}
+		
+
+	
+	
