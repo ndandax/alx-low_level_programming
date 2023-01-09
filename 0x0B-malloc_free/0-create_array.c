82b9182f6034bfdbc116e8fb2d1137061e1db83d
ndandax <sebandandala99@gmail.com>
2023-01-09 06:40:57 -0500

    6

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,23 +10,23 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ar;
+
 	ar = malloc(size * sizeof(char));
 
+	if (size == 0)
+	{
+			return (NULL);
+	}
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
 		ar[i] = c;
-
-		if (size == 0)
-		{
-			return (NULL);
-		}
 	}
-
-	if (ar == NULL)
-		return (NULL);
-
-	ar[i] = '\0';
 
 	return (ar);
 }
