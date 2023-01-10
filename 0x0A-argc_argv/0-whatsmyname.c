fd2fa1b6939d8c8fa6d9c1ad2002c283cd176e45
ndandax <sebandandala99@gmail.com>
2023-01-10 13:08:15 -0500

    3

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,8 @@
  *@argv: array of arguments
  *Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc _attribute_((unused)), char *argv[])
 {
-	void(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
