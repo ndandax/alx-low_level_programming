6bbabb479d31f6e0cb9123f59671fe2c9bc98c9a
ndandax <sebandandala99@gmail.com>
2023-01-10 13:31:29 -0500

    9

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,8 @@
  * @argv: Array name
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void) argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
