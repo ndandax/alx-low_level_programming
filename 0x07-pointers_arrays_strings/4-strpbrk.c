850a772169613542478065323b5d84e4ad8887d6
ndandax <sebandandala99@gmail.com>
2023-01-14 03:14:56 -0500

    r

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,14 @@
 #include "main.h"
+#include "string.h"
+
 /**
- *_strpbrk - Function name
- *@s: string
- *@accept: string
- *Return: pointer to the bytes in s
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: segment to be returned
+ * @accept: value to be computed
+ *
+ * Return: pointer to the byte in s
  */
 char *_strpbrk(char *s, char *accept)
 {
-	char *sc;
-
-	for (sc = s; *sc != '\0'; ++sc)
-	{
-		char *a;
-
-		for (a = accept; *a != '\0'; ++a)
-		{
-			if (*a == *sc)
-			{
-				return ((char *)sc);
-			}
-		}
-	}
-
-	return (NULL);
+	return (strpbrk(s, accept));
 }
