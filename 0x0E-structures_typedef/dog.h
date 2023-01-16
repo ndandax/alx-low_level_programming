8a8bfe9601cea0aaef1cad1b4c67a02356035cb7
ndandax <sebandandala99@gmail.com>
2023-01-16 08:46:57 -0500

    o

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-typedef sruct dog dog_t;
-
+/**
+ * struct dog - structure for dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: pet's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*DOG_H*/
