1c41b29a65ec791e4ef7602f3100b42bbc00aab3
ndandax <sebandandala99@gmail.com>
2023-01-16 10:45:53 -0500

    f

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *init_dog - Function name
+ *@d: d
+ *@name: name
+ *@age: age
+ *@owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
