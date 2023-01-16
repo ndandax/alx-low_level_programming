bfee4655b9ab5318e13afcd5e9dd6edb56630642
ndandax <sebandandala99@gmail.com>
2023-01-16 12:02:45 -0500

    g

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *new_dog - Function
+ *@name: name
+ *@age: age
+ *@owner: owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_dog - Function name
+ *@d: d
+ *Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+	}
+}
