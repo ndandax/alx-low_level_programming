ecdbfcfdc6e9c4851155659fe572a1339a681f9e
ndandax <sebandandala99@gmail.com>
2023-01-17 14:26:18 -0500

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,9 +18,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (dog == NULL)
 		return (NULL);
+
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
+
+	free(dog);
 
 	return (dog);
 }
