af44a58f747e548f65f5516934a10761252af2cb
ndandax <sebandandala99@gmail.com>
2023-01-27 14:35:35 +0000

    list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *print_list - Function name
+ *@h - parameter
+ *Return: 0
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return;
+	printf("%s\n %d\n",h->str,h->len);
+	return (0);
+}
