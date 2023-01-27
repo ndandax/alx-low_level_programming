f7ce76e96973a33139010ec7ba4df5faf39b4f10
ndandax <sebandandala99@gmail.com>
2023-01-27 14:56:03 +0000

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,7 +15,7 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-#include <stdlib.h>
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
